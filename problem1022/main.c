dfb56281e054baebbcbf1dac362bc7d377388c5a
jianglin-wu <wjiangl.id@gmail.com>
2019-08-25 12:14:47 +0800

    perf: 优化遍历方法

--- problem1022/main.c
+++ problem1022/main.c
@@ -2,43 +2,41 @@
 // https://www.dotcpp.com/oj/problem1022.html
 
 #include <stdio.h>
+#include <stdbool.h>
 
-int isPrimeNum(int num)
+// 是否是素数
+bool isPrimeNum(int num)
 {
-	int i = 2;
-	int v;
-	int flag = 0;
-	while(i < num) {
-		v = num % i;
-		if (v == 0) {
-			flag = 1;
+	bool flag = false;
+	for (int i = 2; i < num / 2 + 1; i++) {
+		if (num % i == 0) {
+			flag = true;
 			break;
 		}
-		i++;
+
 	}
 	return flag;
 }
 
+
 int main()
 {
 	int num = 0;
+	int max = 9999;
 
 	printf("请输入一个数：\n");
 	scanf("%d", &num);
-    
-    if (num <= 0 || num >= 200) {
-    	printf("输入 [%d] 值无效，请输入 0-20 的值\n", num);
-    	return 1;
-    }
 
-    int values[num];
-    for (int i = 1; i < num; i++) {
-    	int res = isPrimeNum(i);
+	if (num <= 0 || num >= max) {
+		printf("输入 [%d] 值无效，请输入 1-%d 的值\n", num, max);
+		return 1;
+	}
 
-    	if (res == 0) {
-		    printf("数值 %d 是素数\n", i);
-    	}
-    }
+	for (int i = 1; i < num; i++) {
+		if (!isPrimeNum(i)) {
+			printf("数值 %d 是素数\n", i);
+		}
+	}
 
 	return 0;
 }
