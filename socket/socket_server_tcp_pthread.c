cd9654f7cae057d9b2a02ebb9d78a6ecaa35a747
jianglin-wu <wjiangl.id@gmail.com>
2023-05-15 16:04:48 +0800

    feat

--- socket/socket_server_tcp_pthread.c
+++ socket/socket_server_tcp_pthread.c
@@ -7,10 +7,13 @@
 #include <netinet/ip.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <errno.h>
 
-#define PORT 1027		//端口号
-#define BACKLOG 50	//最大监听数
+#define PORT 1025		//端口号
+#define BACKLOG 1	//最大监听数
 
+//发送内容，参数分别是连接句柄，内容，大小，其他信息（设为0即可） 
+char *str = "HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: 5\r\nContent-Type: text/html\r\n\r\nok!\r\n";
 
 typedef struct {
     int value;
@@ -24,58 +27,38 @@ void *ThreadFun(void *arg)
 	int iRecvLen;
 	char *buf = (char *) malloc(4096);
     printf("[New Thread] fd=%d\n", new_fd);
-	//发送内容，参数分别是连接句柄，内容，大小，其他信息（设为0即可） 
-	char *str = "HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: 5\r\nContent-Type: text/html\r\n\r\nok!\r\n";
 
-    while (1) {
+	do {
 		iRecvLen = recv(new_fd, buf, 4096, 0);	
-		if(0 >= iRecvLen)    //对端关闭连接 返回0
-		{	
+		if(0 >= iRecvLen) {	
+			//对端关闭连接 返回0
 			printf("接收失败或者对端关闭连接！\n");
-			close(new_fd);
-			free(buf);
 			break;
-		}else{
+		} else {
 			printf("[recv] %s\n", buf);
 			send(new_fd, str, strlen(str), 0);
 			printf("[send] %s\n", str);
-			close(new_fd);
-			free(buf);
-			break;
 		}
-	}
+    // 此处 while 为 true 表示保持 TCP 连接，为 false 表示响应数据后立即断开连接
+	} while(0);
 
-    // while (1) {
-	// 	iRecvLen = recv(new_fd, buf, 4096, 0);	
-	// 	if(0 >= iRecvLen)    //对端关闭连接 返回0
-	// 	{	
-	// 		printf("接收失败或者对端关闭连接！\n");
-	// 		close(new_fd);
-	// 		free(buf);
-	// 		break;
-	// 	}else{
-	// 		printf("buf: %s\n", buf);
-	// 		send(new_fd, str, strlen(str), 0);
-	// 	}
-	// }
+	close(new_fd);
+	free(buf);
+	free(data);
 }
 
 int main(void)
 {  
 	int iSocketFD = 0;  //socket句柄
-	int i = 0;
+	socklen_t socklen = 0;
 	struct sockaddr_in stLocalAddr = {0}; //本地地址信息结构图，下面有具体的属性赋值
 	struct sockaddr_in stRemoteAddr = {0}; //对方地址信息
 
-	socklen_t socklen = 0;
-	int len = 20;
-	DATA **plist = malloc(sizeof(DATA) * len);
- 
 	iSocketFD = socket(AF_INET, SOCK_STREAM, 0); //建立socket
 	if(0 > iSocketFD)
 	{
-		printf("创建socket失败！\n");
-		return 0;
+		fprintf(stderr, "[创建 Socket 失败] [errno=%d]: %s\n", errno, strerror(errno));
+		return 1;
 	}
  
 	stLocalAddr.sin_family = AF_INET;  /*该属性表示接收本机或其他机器传输*/
@@ -84,36 +67,30 @@ int main(void)
 	//绑定地址结构体和socket
 	if(0 > bind(iSocketFD, (void *)&stLocalAddr, sizeof(stLocalAddr)))
 	{
-		printf("绑定失败！\n");
-		return 0;
+		fprintf(stderr, "[绑定失败] [errno=%d]: %s\n", errno, strerror(errno));
+		return 1;
 	}
  
 	//开启监听 ，第二个参数是最大监听数
 	if(0 > listen(iSocketFD, BACKLOG))
 	{
-		printf("监听失败！\n");
-		return 0;
+		fprintf(stderr, "[监听失败] [errno=%d]: %s\n", errno, strerror(errno));
+		return 1;
 	}
  
 	printf("iSocketFD: %d\n", iSocketFD);	
 	//在这里阻塞知道接收到消息，参数分别是socket句柄，接收到的地址信息以及大小 
 	while (1) {
-		if (i >= len) {
-            plist = (DATA **)realloc(plist, 5);
-            len += 5;
-            printf("len=%d\n", len);
-        }
-		plist[i] = (DATA *)malloc(sizeof(DATA));
-		plist[i]->value = accept(iSocketFD, (void *)&stRemoteAddr, &socklen);
-		if(0 > plist[i]->value)
+		DATA *data = (DATA *)malloc(sizeof(DATA));
+		data->value = accept(iSocketFD, (void *)&stRemoteAddr, &socklen);
+		if(0 > data->value)
 		{
 			printf("接收失败！\n");
 			return 0;
-		}else{
-			printf("\n[accept] plist[i]->value: %d\n", plist[i]->value);
-			int res = pthread_create(&plist[i]->fd, NULL, ThreadFun, &plist[i]->value);
-			printf("pthread_create plist[i]->value=%d res=%d\n", plist[i]->value, res);
+		}else {
+			printf("\n[accept] data->value: %d\n", data->value);
+			int res = pthread_create(&data->fd, NULL, ThreadFun, &data->value);
+			printf("pthread_create data->value=%d res=%d\n", data->value, res);
 		}
-		i++;
 	}
 }
